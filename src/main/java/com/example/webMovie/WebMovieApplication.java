764d1e6d1dddb9e88e1f78dd1c7508b1fc8f62b9
jinah <j_happy-a@naver.com>
2021-03-25 23:06:06 +0900

    spring 환경구현

---
+++ src/main/java/com/example/webMovie/WebMovieApplication.java
@@ -0,0 +1,13 @@
+package com.example.webMovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebMovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebMovieApplication.class, args);
+	}
+
+}
